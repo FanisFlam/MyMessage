509fff0c95f1af27649e047f5c5fdbb92f2678eb
FanisFlam <fanisflamofficial@gmail.com>
2024-03-25 17:59:08 +0200

    better syntax

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
-app.get('/', (req ,res) => {
+app.get('/', (req, res) => {
     res.render('index');
 });
 
 app.use((req, res, next) => {
-    res.status(404)
-        .send('404 - Not found');
+    res.status(404).send(`404 - not found`);
 });
 
 app.listen(PORT, () => {
-    console.log(`App is listening on ${PORT}`);
+    console.log(`App is listening to ${PORT}`);
 });
